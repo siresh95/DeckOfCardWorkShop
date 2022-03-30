e8f0d28b571c3b24d97a2f857ccc33d066cc9a28
reshsingh1995@gmail.com <reshsingh1995@gmail.com>
2022-03-30 19:57:54 +0530

    UC2 : Moving forward we need to have more than 2 players and Max 4 Players, Create Players Class to add Players from console.

---
+++ src/com/bridgelabz/NumberOfPlayer.java
@@ -0,0 +1,23 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class NumberOfPlayer {
+
+    public void noOfPlayers() {
+
+        System.out.print("\nEnter number of players minimum 2 , maximum 4 : ");
+        Scanner sc = new Scanner(System.in);
+        int player = sc.nextInt();
+
+        if (player >= 2 && player <= 4) {
+            System.out.println("\n" + player + " players will play the game");
+        }
+        else {
+            System.out.println("Please enter number of players in the Range");
+            noOfPlayers();
+        }
+    }
+
+    }
+
